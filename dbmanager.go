3a9909d36d0e0ea0e3bdb183f1ea5a884e21bf00
PrakashNatarajan <prakashntrjn@gmail.com>
2018-09-20 17:27:48 +0530

    Changed the color from name to hexacode.

--- dbmanager.go
+++ dbmanager.go
@@ -63,7 +63,7 @@ func (manager *DBManager) GetColorCode(clrId int) (code string) {
 func (manager *DBManager) GetShapesColorsUsers(limit, offset int) (shapesgraphics []ShapeGraphic) {
     sLimit := strconv.Itoa(limit)
     sOffset := strconv.Itoa(offset)
-    query := "SELECT shapes.id AS shape_id, shapes.name AS shape_name, IFNULL(graphics_colors_users.color_code, 'tomato') AS color_code, IFNULL(graphics_colors_users.user_name, ' ') AS user_name, IFNULL(graphics_colors_users.last_updated_at, ' ') AS last_updated_at FROM shapes LEFT OUTER JOIN (SELECT graphics.shape_id AS grp_shape_id, colors.code AS color_code, users.name AS user_name, MAX(graphics.updated_at) AS last_updated_at FROM graphics INNER JOIN colors ON graphics.color_id = colors.id INNER JOIN users ON graphics.user_id = users.id GROUP BY graphics.shape_id) AS graphics_colors_users ON shapes.id = graphics_colors_users.grp_shape_id LIMIT " + sLimit + " OFFSET " + sOffset
+    query := "SELECT shapes.id AS shape_id, shapes.name AS shape_name, IFNULL(graphics_colors_users.color_code, '#ff6347') AS color_code, IFNULL(graphics_colors_users.user_name, ' ') AS user_name, IFNULL(graphics_colors_users.last_updated_at, ' ') AS last_updated_at FROM shapes LEFT OUTER JOIN (SELECT graphics.shape_id AS grp_shape_id, colors.code AS color_code, users.name AS user_name, MAX(graphics.updated_at) AS last_updated_at FROM graphics INNER JOIN colors ON graphics.color_id = colors.id INNER JOIN users ON graphics.user_id = users.id GROUP BY graphics.shape_id) AS graphics_colors_users ON shapes.id = graphics_colors_users.grp_shape_id LIMIT " + sLimit + " OFFSET " + sOffset
     log.Println(query)
     rows, err := manager.database.Query(query) 
     if err != nil {
@@ -72,14 +72,14 @@ func (manager *DBManager) GetShapesColorsUsers(limit, offset int) (shapesgraphic
     }
     for rows.Next() {
         var shape_id int
-        var shapeName, colorCode, userName, lastUpdatedAt string
-        err = rows.Scan(&shape_id, &shapeName, &colorCode, &userName, &lastUpdatedAt)
+        var shape_name, color_code, user_name, last_updated_at string
+        err = rows.Scan(&shape_id, &shape_name, &color_code, &user_name, &last_updated_at)
         if err != nil {
             log.Println(err)
             return
         }
         
-        shgrp := ShapeGraphic{ShapeId: shape_id, ShapeName: shapeName, ColorCode: colorCode, UserName: userName, LastUpdatedAt: lastUpdatedAt}
+        shgrp := ShapeGraphic{ShapeId: shape_id, ShapeName: shape_name, ColorCode: color_code, UserName: user_name, LastUpdatedAt: last_updated_at}
         shapesgraphics = append(shapesgraphics, shgrp)
     }
     rows.Close()
