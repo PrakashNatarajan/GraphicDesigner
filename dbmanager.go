0655c5ddc4acaef2a68eab20d8d7dc3ebe71590a
PrakashNatarajan <prakashntrjn@gmail.com>
2018-09-22 23:06:27 +0530

    Added the strftime time format in the sql.

--- dbmanager.go
+++ dbmanager.go
@@ -68,7 +68,7 @@ func (manager *DBManager) GetColorShapeUser(shpId, clrId, usrId int) (shpClrUsr 
 func (manager *DBManager) GetShapesColorsUsers(limit, offset int) (shapesgraphics []ShapeGraphic) {
     sLimit := strconv.Itoa(limit)
     sOffset := strconv.Itoa(offset)
-    query := "SELECT shapes.id AS shape_id, shapes.name AS shape_name, IFNULL(graphics_colors_users.color_code, '#ff6347') AS color_code, IFNULL(graphics_colors_users.user_name, ' ') AS user_name, IFNULL(graphics_colors_users.last_updated_at, ' ') AS last_updated_at FROM shapes LEFT OUTER JOIN (SELECT graphics.shape_id AS grp_shape_id, colors.code AS color_code, users.name AS user_name, MAX(graphics.updated_at) AS last_updated_at FROM graphics INNER JOIN colors ON graphics.color_id = colors.id INNER JOIN users ON graphics.user_id = users.id GROUP BY graphics.shape_id) AS graphics_colors_users ON shapes.id = graphics_colors_users.grp_shape_id LIMIT " + sLimit + " OFFSET " + sOffset
+    query := "SELECT shapes.id AS shape_id, shapes.name AS shape_name, IFNULL(graphics_colors_users.color_code, '#ff6347') AS color_code, IFNULL(graphics_colors_users.user_name, ' ') AS user_name, IFNULL(graphics_colors_users.last_updated_at, ' ') AS last_updated_at FROM shapes LEFT OUTER JOIN (SELECT graphics.shape_id AS grp_shape_id, colors.code AS color_code, users.name AS user_name, strftime('%Y-%m-%d %H:%M:%S', MAX(graphics.updated_at)) AS last_updated_at FROM graphics INNER JOIN colors ON graphics.color_id = colors.id INNER JOIN users ON graphics.user_id = users.id GROUP BY graphics.shape_id) AS graphics_colors_users ON shapes.id = graphics_colors_users.grp_shape_id LIMIT " + sLimit + " OFFSET " + sOffset
     log.Println(query)
     rows, err := manager.database.Query(query) 
     if err != nil {
